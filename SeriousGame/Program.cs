7813cf04ef97fef0dbbf5a61d09a36bd6da1b65e
ImaSkrrtman <118083813+ImaSkrrtman@users.noreply.github.com>
2024-12-04 18:50:26 +0100

    Mam nadzieje ze zadziala
    pliska

--- SeriousGame/Program.cs
+++ SeriousGame/Program.cs
@@ -12,6 +12,7 @@ namespace SeriousGame
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+            //zmiana kolejna
         }
     }
 }
